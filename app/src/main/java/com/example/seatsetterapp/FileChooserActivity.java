e0696e9ee6b9ec090b6cedaffb91464504f7326b
Alexis Oswald <alexisoswald98@gmail.com>
2019-04-30 17:25:58 -0400

    Seates are generated, but last name in list is repeated

--- app/src/main/java/com/example/seatsetterapp/FileChooserActivity.java
+++ app/src/main/java/com/example/seatsetterapp/FileChooserActivity.java
@@ -3,14 +3,11 @@ package com.example.seatsetterapp;
 import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
@@ -21,49 +18,25 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
-import android.database.Cursor;
-import android.provider.MediaStore;
-import android.content.CursorLoader;
-
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 public class FileChooserActivity extends AppCompatActivity {
 
     //Declaration of variables
     private static final String TAG = "MainActivity";
+    List<String> allNames = new ArrayList<>();
 
-    private String[] FilePathStrings;
-    private String[] FileNameStrings;
-    private File[] listFile;
-    File file;
-
-    Button btnUpDirectory, btnSDCard;
-
-    ArrayList<String> pathHistory;
-    String lastDirectory;
-    int count = 0;
-
-    ArrayList<XValues> uploadData;      //arraylist<XValues> ??
-
-
-    ListView lvInternalStorage;
+    ArrayList<String> names = new ArrayList<>(); //ArrayList that contains all names from excel file
 
     private void readExcelData(Uri selectedFile) {
-        Log.d(TAG, "readExceldata: Reading Excel File.");
-        //declare input file
-        //File inputFile = new File(filePath);
-
-
         try {
             InputStream inputStream = getContentResolver().openInputStream(selectedFile);
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
@@ -84,17 +57,15 @@ public class FileChooserActivity extends AppCompatActivity {
                         toastMessage("Error excel file format incorrect");
                         break;
                     } else {
-                        String value = getCellAsString(row, c, formulaEvaluator);//writing getcellasstring later
-                        String cellInfo = "r:" + r + "; c:" + c + "; v:" + value;
-                        Log.d(TAG, "readExcelData: Data from row: " + cellInfo);
+                        String value = getCellAsString(row, c, formulaEvaluator);
                         sb.append(value + ", ");
                     }
                 }
-                sb.append(";");
             }
 
-            Log.d(TAG, "ReadExcelData: STRINGBUILDER: " + sb.toString());
-            parseStringBuilder(sb);
+            Log.d(TAG, "ReadExcelData: STRINGBUILDER: " + sb.toString().replace(";",""));
+            names.add(sb.toString().replace(";","")); // where the names are added into the arraylist
+            Log.d(TAG, "NAMES HERE " + names.toString());
 
         } catch (FileNotFoundException e) {
             Log.e(TAG, "readExcelData: FileNotFoundException " + e.getMessage());
@@ -102,48 +73,6 @@ public class FileChooserActivity extends AppCompatActivity {
             Log.e(TAG, "readExcelData: Error reading inputstream" + e.getMessage());
         }
     }
-
-    public void parseStringBuilder(StringBuilder mStringBuilder) {
-        Log.d(TAG, "parseStringBuilder: Started parsing ");
-
-
-        //Add
-
-        //splits sb into rows
-        String[] rows = mStringBuilder.toString().split(":");
-
-        //add to arraylist<Xvalues> row by row
-        for (int i = 0; i < rows.length; i++) {
-            String[] columns = rows[i].split(",");
-
-            //use try catch to make sure no '' that try to parse into doubles
-            try {
-                String x = (columns[0]);
-                String y = (columns[1]);
-
-                String cellInfo = "(x.y): (" + x + "," + y + ")";
-                Log.d(TAG, "ParseStringBuilder: Data from row: " + cellInfo);
-
-                //add data to ArrayList
-                uploadData.add(new XValues(x, y));
-            } catch (NumberFormatException e) {
-                Log.e(TAG, "parseStringuilder: NumberFormatException " + e.getMessage());
-            }
-
-        }
-    }
-
-    private void printDataToLog() {
-        Log.d(TAG, "PrintDataToLog: Printing data to log....");
-
-        for (int i = 0; i < uploadData.size(); i++) {
-            String x = uploadData.get(i).getX();
-            String y = uploadData.get(i).getY();
-            Log.d(TAG, "printDataToLog: (x,y): (" + x + "," + y + ")");
-        }
-    }
-
-
     private String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
         String value = "";
         try {
@@ -173,42 +102,6 @@ public class FileChooserActivity extends AppCompatActivity {
         }
         return value;
     }
-
-
-    private void checkInternalStorage() {
-        Log.d(TAG, "checkInternalStorage Started");
-        try {
-            if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                toastMessage("No sd card found");
-            } else {
-                //locate image folder in your sd card
-                file = new File((pathHistory.get(count)));
-                Log.d(TAG, "checkInternalStorage:directory path: " + pathHistory.get(count));
-            }
-
-            listFile = file.listFiles();
-            //Create string array for file path strings
-            FilePathStrings = new String[listFile.length];
-
-            //string array for file name strings
-            FileNameStrings = new String[listFile.length];
-            for (int i = 0; i < listFile.length; i++) {//path of image file
-                FilePathStrings[i] = listFile[i].getAbsolutePath();
-                //get name of image file
-                FileNameStrings[i] = listFile[i].getName();
-            }
-
-            for (int i = 0; i < listFile.length; i++) {
-                Log.d("Files", "FileName: " + listFile[i].getName());
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, FilePathStrings);
-            lvInternalStorage.setAdapter(adapter);
-
-        } catch (NullPointerException e) {
-            Log.e(TAG, "checkInternalStorage: NULLPOINTER EXCEPTION " + e.getMessage());
-        }
-    }
-
     private void checkFilePermissions() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             int permissionCheck = this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
@@ -220,14 +113,98 @@ public class FileChooserActivity extends AppCompatActivity {
             }
         }
     }
-
-
-    /*
-    customizable toast
-     */
-
     private void toastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private List<String> parseArrayList(){
+        String all = names.get(0);
+        allNames = Arrays.asList(all.split("\\s*,\\s*"));
+        return allNames;
+    }
+    private void generateSeatingChart(){
+        parseArrayList();
+        ArrayList<Integer> numberList = new ArrayList<>();
+        for (int i = 1; i < allNames.size(); i++) {
+            numberList.add(i);
+        }
+
+        Collections.shuffle(numberList); //generates random numbers to correspond with the names
+        setAllTextViews(numberList);
+
+    }
+
+    private void setAllTextViews(ArrayList<Integer> numberList){
+        
+        TextView T1 = (TextView)findViewById(R.id.T1);
+        TextView T2 = (TextView)findViewById(R.id.T2);
+        TextView T3 = (TextView)findViewById(R.id.T3);
+        TextView T4 = (TextView)findViewById(R.id.T4);
+        TextView T5 = (TextView)findViewById(R.id.T5);
+        TextView T6 = (TextView)findViewById(R.id.T6);
+        TextView T7 = (TextView)findViewById(R.id.T7);
+        TextView T8 = (TextView)findViewById(R.id.T8);
+        TextView T9 = (TextView)findViewById(R.id.T9);
+        TextView T10 = (TextView)findViewById(R.id.T10);
+        TextView T11 = (TextView)findViewById(R.id.T11);
+        TextView T12 = (TextView)findViewById(R.id.T12);
+        TextView T13 = (TextView)findViewById(R.id.T13);
+        TextView T14 = (TextView)findViewById(R.id.T14);
+        TextView T15 = (TextView)findViewById(R.id.T15);
+        TextView T16 = (TextView)findViewById(R.id.T16);
+        TextView T17= (TextView)findViewById(R.id.T17);
+        TextView T18 = (TextView)findViewById(R.id.T18);
+        TextView T19 = (TextView)findViewById(R.id.T19);
+        TextView T20 = (TextView)findViewById(R.id.T20);
+        TextView T21 = (TextView)findViewById(R.id.T21);
+        TextView T22 = (TextView)findViewById(R.id.T22);
+        TextView T23 = (TextView)findViewById(R.id.T23);
+        TextView T24 = (TextView)findViewById(R.id.T24);
+        TextView T25= (TextView)findViewById(R.id.T25);
+        TextView T26= (TextView)findViewById(R.id.T26);
+        TextView T27= (TextView)findViewById(R.id.T27);
+        TextView T28= (TextView)findViewById(R.id.T28);
+        TextView T29= (TextView)findViewById(R.id.T29);
+        TextView T30= (TextView)findViewById(R.id.T30);
+
+       if(numberList.size() < 30){
+           for(int i = numberList.size(); i < 30; i++){
+               numberList.add(0);
+           }
+       }
+
+        T1.setText("   "+ allNames.get(numberList.get(0)) + "   ");
+        T2.setText("   "+ allNames.get(numberList.get(1)) + "   ");
+        T3.setText("   "+ allNames.get(numberList.get(2)) + "   ");
+        T4.setText("   "+ allNames.get(numberList.get(3)) + "   ");
+        T5.setText("   "+ allNames.get(numberList.get(4)) + "   ");
+        T6.setText("   "+ allNames.get(numberList.get(5)) + "   ");
+        T7.setText("   "+ allNames.get(numberList.get(6)) + "   ");
+        T8.setText("   "+ allNames.get(numberList.get(7)) + "   ");
+        T9.setText("   "+ allNames.get(numberList.get(8)) + "   ");
+        T10.setText("   "+ allNames.get(numberList.get(9)) + "   ");
+        T11.setText("   "+ allNames.get(numberList.get(10)) + "   ");
+        T12.setText("   "+ allNames.get(numberList.get(11)) + "   ");
+        T13.setText("   "+ allNames.get(numberList.get(12)) + "   ");
+        T14.setText("   "+ allNames.get(numberList.get(13)) + "   ");
+        T15.setText("   "+ allNames.get(numberList.get(14)) + "   ");
+        T16.setText("   "+ allNames.get(numberList.get(15)) + "   ");
+        T17.setText("   "+ allNames.get(numberList.get(16)) + "   ");
+        T18.setText("   "+ allNames.get(numberList.get(17)) + "   ");
+        T19.setText("   "+ allNames.get(numberList.get(18)) + "   ");
+        T20.setText("   "+ allNames.get(numberList.get(19)) + "   ");
+        T21.setText("   "+ allNames.get(numberList.get(20)) + "   ");
+        T22.setText("   "+ allNames.get(numberList.get(21)) + "   ");
+        T23.setText("   "+ allNames.get(numberList.get(22)) + "   ");
+        T24.setText("   "+ allNames.get(numberList.get(23)) + "   ");
+        T25.setText("   "+ allNames.get(numberList.get(24)) + "   ");
+        T26.setText("   "+ allNames.get(numberList.get(25)) + "   ");
+        T27.setText("   "+ allNames.get(numberList.get(26)) + "   ");
+        T28.setText("   "+ allNames.get(numberList.get(27)) + "   ");
+        T29.setText("   "+ allNames.get(numberList.get(28)) + "   ");
+        T30.setText("   "+ allNames.get(numberList.get(29)) + "   ");
+
+
     }
 
     @Override
@@ -247,10 +224,9 @@ public class FileChooserActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 123 && resultCode == RESULT_OK) {
             Uri selectedFile = data.getData(); //The uri with the location of the file
-            String absolutePath = getContentResolver().getType(selectedFile);
-            checkFilePermissions();
-
-            readExcelData(selectedFile);
+            checkFilePermissions(); //checks permissions
+            readExcelData(selectedFile); // reads the excel file
+            generateSeatingChart();
         }
 
     }
